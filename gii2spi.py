9ab2cf37297a021df6356d8b78157ee47158b6bc
Manik Bhattacharjee <manik.bhattacharjee@free.fr>
2018-04-14 05:32:22 +0200

    Convert a gii mesh into a SPI point cloud file for Cartool EEG software - not generic yet

---
+++ gii2spi.py
@@ -0,0 +1,13 @@
+from soma import aims
+
+m = aims.read("/home/manik/full.gii")
+
+idx = 0
+f = open("/home/manik/full.spi", "w")
+for v in m.vertex():
+   f.write(str(v[0]-93.0)+ "     "+str(-v[1]-167+283) + "      " + str(-v[2]-163+264) + "     LAS"+str(idx)+"\n")
+   idx += 1
+
+f.close()
+
+
